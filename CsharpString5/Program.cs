1c9de13410ecd83e8acd25c701b563f7489a3232
beverley-dick <beverleydick11@gmail.com>
2023-05-31 09:36:57 +0300

    Add project files.

---
+++ CsharpString5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CsharpString5
+{
+    class Test
+    {
+       public static void Main(string[] args)
+        {
+            //create string 
+
+            string name = "Programiz";
+
+            //string interpolation
+            string message = $"Welcome to {name}";
+            Console.WriteLine(message);
+
+            Console.ReadLine();
+        }
+    }
+}
